e670c60157a93b8e52e568bdb061c1db6027ca24
hfriedrich <heiko.friedrich@researchstudio.at>
2018-05-07 13:44:04 +0200

    change AdManager to class

--- admanager/AdManager.py
+++ admanager/AdManager.py
@@ -17,115 +17,134 @@ from facebookads.specs import ObjectStorySpec, LinkData
 import json
 import os
 
-def initSession():
-    this_dir = os.path.dirname(__file__)
-    config_filename = os.path.join(this_dir, 'config.json')
+class AdManager:
 
-    config_file = open(config_filename)
-    config = json.load(config_file)
-    config_file.close()
+    def __init__(self, account_id, business_id):
+        this_dir = os.path.dirname(__file__)
+        config_filename = os.path.join(this_dir, 'config.json')
+        config_file = open(config_filename)
+        config = json.load(config_file)
+        config_file.close()
 
-    session = FacebookSession(
-        config['app_id'],
-        config['app_secret'],
-        config['access_token'],
-    )
-    api = FacebookAdsApi(session)
-    FacebookAdsApi.set_default_api(api)
-    return api
+        session = FacebookSession(
+            config['app_id'],
+            config['app_secret'],
+            config['access_token'],
+        )
+        api = FacebookAdsApi(session)
+        FacebookAdsApi.set_default_api(api)
 
-def listAllCampaigns(account_id):
-    my_account = AdAccount(fbid=account_id)
-    my_account.remote_read(fields=[Campaign.Field.id, Campaign.Field.name])
-    return my_account.get_campaigns()
+        self.api = api
+        self.account_id = account_id
+        self.business_id = business_id
+        self.pageId = config['page_id']
 
-def deleteAllCampaigns(account_id):
-    my_account = AdAccount(fbid=account_id)
-    my_account.remote_read()
-    for c in my_account.get_campaigns():
-        c.remote_delete()
+    def listAllCampaigns(self):
+        my_account = AdAccount(fbid=self.account_id)
+        my_account.remote_read(fields=[Campaign.Field.id, Campaign.Field.name])
+        return my_account.get_campaigns()
 
-def readCampaign(campaign_id):
-    campaign = Campaign(fbid=campaign_id)
-    campaign.remote_read(fields=[Campaign.Field.id, Campaign.Field.name])
-    return campaign
+    def deleteAllCampaigns(self):
+        my_account = AdAccount(fbid=self.account_id)
+        my_account.remote_read()
+        for c in my_account.get_campaigns():
+            c.remote_delete()
 
-def createCampaign(account_id):
-    campaign = Campaign(parent_id = account_id)
-    campaign[Campaign.Field.name] = "Matchat Campain"
-    campaign[Campaign.Field.objective] = Campaign.Objective.link_clicks
-    campaign[Campaign.Field.configured_status] = Campaign.Status.paused
-    campaign.remote_create()
-    return campaign
+    def readCampaign(self, campaign_id):
+        campaign = Campaign(fbid=campaign_id)
+        campaign.remote_read(fields=[Campaign.Field.id, Campaign.Field.name])
+        return campaign
 
-def readAdSet(adset_id):
-    ad_set = AdSet(fbid=adset_id)
-    ad_set.remote_read(fields=[AdSet.Field.id])
-    return ad_set
+    def createCampaign(self):
+        campaign = Campaign(parent_id = self.account_id)
+        campaign[Campaign.Field.name] = "Matchat Campain"
+        campaign[Campaign.Field.objective] = Campaign.Objective.link_clicks
+        campaign[Campaign.Field.configured_status] = Campaign.Status.paused
+        campaign.remote_create()
+        return campaign
 
-def createAdSet(account_id, campaign_id):
-    ad_set = AdSet(parent_id=account_id)
-    ad_set[AdSet.Field.name] = 'Matchat AdSet'
+    def listAllAdsets(self):
+        adsets = []
+        campaigns = self.listAllCampaigns()
+        for c in campaigns:
+            adsets.append(c.get_ad_sets([AdSet.Field.id, AdSet.Field.name]))
+        return adsets
 
-    targeting = {}
-    targeting[Targeting.Field.age_max] = 50
-    targeting[Targeting.Field.age_min] = 18
-    targeting[Targeting.Field.geo_locations] = {
-        'countries': ['AT']
-    }
-    ad_set[AdSet.Field.targeting] = targeting
-    ad_set[AdSet.Field.campaign_id] = campaign_id
-    ad_set[AdSet.Field.is_autobid] = True
-    ad_set[AdSet.Field.daily_budget] = 100 # US cents
-    ad_set[AdSet.Field.billing_event] = AdSet.BillingEvent.impressions
-    ad_set.remote_create()
-    return ad_set
+    def readAdSet(adset_id):
+        ad_set = AdSet(fbid=adset_id)
+        ad_set.remote_read(fields=[AdSet.Field.id])
+        return ad_set
 
-def createAd(account_id, adset_id, page_id):
-    ad = Ad(parent_id=account_id)
-    ad[Ad.Field.name] = ' Matchat Ad'
-    ad[Ad.Field.adset_id] = adset_id
+    def createAdSet(self, campaign_id):
+        ad_set = AdSet(parent_id=self.account_id)
+        ad_set[AdSet.Field.name] = 'Matchat AdSet'
 
-    link_data = LinkData()
-    link_data[LinkData.Field.message] = 'Great looking SXT handbags in store. #prettybag'
-    link_data[LinkData.Field.link] = 'http://example.com'
-    link_data[LinkData.Field.caption] = 'www.example.com'
-    #link_data[LinkData.Field.image_hash] = '<IMAGE_HASH>'
+        targeting = {}
+        targeting[Targeting.Field.age_max] = 50
+        targeting[Targeting.Field.age_min] = 18
+        targeting[Targeting.Field.geo_locations] = {
+            'countries': ['AT']
+        }
+        ad_set[AdSet.Field.targeting] = targeting
+        ad_set[AdSet.Field.campaign_id] = campaign_id
+        ad_set[AdSet.Field.is_autobid] = True
+        ad_set[AdSet.Field.daily_budget] = 100 # US cents
+        ad_set[AdSet.Field.billing_event] = AdSet.BillingEvent.impressions
+        ad_set.remote_create()
+        return ad_set
 
-    call_to_action = {
-        'type': 'LEARN_MORE',
-    }
-    call_to_action['value'] = {
-        'link':'http://example.com',
-    }
+    def listAllAds(self):
+        ads = []
+        campaigns = self.listAllCampaigns()
+        for c in campaigns:
+            ads.append(c.get_ads([Ad.Field.id, Ad.Field.name]))
+        return ads
 
-    link_data[LinkData.Field.call_to_action] = call_to_action
+    def createAd(self, adset_id, page_id):
+        ad = Ad(parent_id=self.account_id)
+        ad[Ad.Field.name] = ' Matchat Ad'
+        ad[Ad.Field.adset_id] = adset_id
 
-    object_story_spec = ObjectStorySpec()
-    object_story_spec[ObjectStorySpec.Field.page_id] = page_id
-    object_story_spec[ObjectStorySpec.Field.link_data] = link_data
+        link_data = LinkData()
+        link_data[LinkData.Field.message] = 'Great looking SXT handbags in store. #prettybag'
+        link_data[LinkData.Field.link] = 'http://example.com'
+        link_data[LinkData.Field.caption] = 'www.example.com'
+        #link_data[LinkData.Field.image_hash] = '<IMAGE_HASH>'
 
-    ad[Ad.Field.creative] = {
-        AdCreative.Field.title: 'try out matchat!',
-        AdCreative.Field.body: 'decentralized online marketplace',
-        #AdCreative.Field.object_url: 'https://matchat.org',
-        AdCreative.Field.object_story_spec: object_story_spec,
-        #AdCreative.Field.image_hash: image_hash,
-    }
+        call_to_action = {
+            'type': 'LEARN_MORE',
+        }
+        call_to_action['value'] = {
+            'link':'http://example.com',
+        }
 
-    ad[Ad.Field.status] = AdSet.Status.paused
-    ad.remote_create()
-    return ad
+        link_data[LinkData.Field.call_to_action] = call_to_action
 
-def createSplitTestAdStudy(business_id, adset1_id, adset2_id):
-    adstudy = AdStudy(parent_id=business_id)
-    adstudy[AdStudy.Field.name] = 'Matchat AdStudy'
-    adstudy[AdStudy.Field.type] = AdStudy.Type.split_test
-    adstudy[AdStudy.Field.start_time] = 1532512562
-    adstudy[AdStudy.Field.end_time] = 1532512563
-    adstudy[AdStudy.Field.cells] = [
-        {'name' : 'Men', 'adsets' : [adset1_id], 'treatment_percentage' : 50.0},
-        {'name' : 'Women', 'adsets' : [adset2_id], 'treatment_percentage' : 50.0}
-    ]
-    adstudy.remote_create()
-    return adstudy
+        object_story_spec = ObjectStorySpec()
+        object_story_spec[ObjectStorySpec.Field.page_id] = page_id
+        object_story_spec[ObjectStorySpec.Field.link_data] = link_data
+
+        ad[Ad.Field.creative] = {
+            AdCreative.Field.title: 'try out matchat!',
+            AdCreative.Field.body: 'decentralized online marketplace',
+            #AdCreative.Field.object_url: 'https://matchat.org',
+            AdCreative.Field.object_story_spec: object_story_spec,
+            #AdCreative.Field.image_hash: image_hash,
+        }
+
+        ad[Ad.Field.status] = AdSet.Status.paused
+        ad.remote_create()
+        return ad
+
+    def createSplitTestAdStudy(self, adset1_id, adset2_id):
+        adstudy = AdStudy(parent_id=self.business_id)
+        adstudy[AdStudy.Field.name] = 'Matchat AdStudy'
+        adstudy[AdStudy.Field.type] = AdStudy.Type.split_test
+        adstudy[AdStudy.Field.start_time] = 1532512562
+        adstudy[AdStudy.Field.end_time] = 1532512563
+        adstudy[AdStudy.Field.cells] = [
+            {'name' : 'Men', 'adsets' : [adset1_id], 'treatment_percentage' : 50.0},
+            {'name' : 'Women', 'adsets' : [adset2_id], 'treatment_percentage' : 50.0}
+        ]
+        adstudy.remote_create()
+        return adstudy
--- test.py
+++ test.py
@@ -3,16 +3,34 @@ from admanager.AdManager import *
 #import admanager.AdManager
 
 if __name__ == '__main__':
-    initSession()
 
     # 472518946157433 = Heiko Friedrich Ad Account
 
-    my_account = AdAccount(fbid='act_212526696146973') # Ad Account 1
+    my_account_id = 'act_212526696146973'
+    my_business_id = '212501799482796'
+
+    admanager = AdManager('act_212526696146973', '212501799482796')
+
+    my_account = AdAccount(fbid=my_account_id) # Ad Account 1
     my_account.remote_read()
     print "account id: " + my_account.get_id_assured()
 
-    campaigns = listAllCampaigns('act_212526696146973')
+    campaigns = admanager.listAllCampaigns()
     print "campaigns: " + str(campaigns)
+
+
+    # campaign = createCampaign(my_account_id)
+    # adset1 = createAdSet(my_account_id, campaign.get_id())
+    # adset2 = createAdSet(my_account_id, campaign.get_id())
+    # createSplitTestAdStudy(my_business_id, adset1.get_id(), adset2.get_id())
+    #createAd(my_account_id, '23842999065780500', '135003680685668')
+
+    adsets = admanager.listAllAdsets()
+    print "adsets: " + str(list(adsets))
+
+    adsets = admanager.listAllAds()
+    print "ads: " + str(list(adsets))
+
 
     # deleteAllCampaigns('act_212526696146973')
     #
