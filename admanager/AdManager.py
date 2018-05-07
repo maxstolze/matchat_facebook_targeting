aefb7e1f2674cd20ee3c6f5f5a2d458f8dae9b42
hfriedrich <heiko.friedrich@researchstudio.at>
2018-05-07 17:56:54 +0200

    use json files to load campaign, adset and ad info

--- admanager/AdManager.py
+++ admanager/AdManager.py
@@ -19,9 +19,8 @@ import os
 
 class AdManager:
 
-    def __init__(self, account_id, business_id):
-        this_dir = os.path.dirname(__file__)
-        config_filename = os.path.join(this_dir, 'config.json')
+    def __init__(self, account_id, business_id, resource_path):
+        config_filename = os.path.join(resource_path, 'config.json')
         config_file = open(config_filename)
         config = json.load(config_file)
         config_file.close()
@@ -34,6 +33,7 @@ class AdManager:
         api = FacebookAdsApi(session)
         FacebookAdsApi.set_default_api(api)
 
+        self.resource_path = resource_path
         self.api = api
         self.account_id = account_id
         self.business_id = business_id
@@ -50,24 +50,38 @@ class AdManager:
         for c in my_account.get_campaigns():
             c.remote_delete()
 
+    def readCampaignFromJson(self, jsonFile):
+        campaign = Campaign(parent_id = self.account_id)
+        config_file = open(jsonFile)
+        config = json.load(config_file)
+        config_file.close()
+        for key in config.keys():
+            campaign[key] = config[key]
+        return campaign
+
+    def readCampaignsFromResources(self):
+        campaigns = []
+        campaigns_dir = os.path.join(self.resource_path, "campaigns")
+        for file in os.listdir(campaigns_dir):
+            if file.endswith(".json"):
+                print os.path.join(campaigns_dir, file)
+                campaign = self.readCampaignFromJson(os.path.join(campaigns_dir, file))
+                campaigns.append(campaign)
+        return campaigns
+
+    def createCampaign(self, campaign):
+        campaign.remote_create()
+
     def readCampaign(self, campaign_id):
         campaign = Campaign(fbid=campaign_id)
         campaign.remote_read(fields=[Campaign.Field.id, Campaign.Field.name])
-        return campaign
-
-    def createCampaign(self):
-        campaign = Campaign(parent_id = self.account_id)
-        campaign[Campaign.Field.name] = "Matchat Campain"
-        campaign[Campaign.Field.objective] = Campaign.Objective.link_clicks
-        campaign[Campaign.Field.configured_status] = Campaign.Status.paused
-        campaign.remote_create()
         return campaign
 
     def listAllAdsets(self):
         adsets = []
         campaigns = self.listAllCampaigns()
         for c in campaigns:
-            adsets.append(c.get_ad_sets([AdSet.Field.id, AdSet.Field.name]))
+            adsets.extend(c.get_ad_sets([AdSet.Field.id, AdSet.Field.name]))
         return adsets
 
     def readAdSet(adset_id):
@@ -75,76 +89,70 @@ class AdManager:
         ad_set.remote_read(fields=[AdSet.Field.id])
         return ad_set
 
-    def createAdSet(self, campaign_id):
-        ad_set = AdSet(parent_id=self.account_id)
-        ad_set[AdSet.Field.name] = 'Matchat AdSet'
+    def readAdsetFromJson(self, jsonFile):
+        adset = AdSet(parent_id = self.account_id)
+        config_file = open(jsonFile)
+        config = json.load(config_file)
+        config_file.close()
+        for key in config.keys():
+            adset[key] = config[key]
+        return adset
 
-        targeting = {}
-        targeting[Targeting.Field.age_max] = 50
-        targeting[Targeting.Field.age_min] = 18
-        targeting[Targeting.Field.geo_locations] = {
-            'countries': ['AT']
-        }
-        ad_set[AdSet.Field.targeting] = targeting
-        ad_set[AdSet.Field.campaign_id] = campaign_id
-        ad_set[AdSet.Field.is_autobid] = True
-        ad_set[AdSet.Field.daily_budget] = 100 # US cents
-        ad_set[AdSet.Field.billing_event] = AdSet.BillingEvent.impressions
-        ad_set.remote_create()
-        return ad_set
+    def readAdsetsFromResources(self):
+        adsets = []
+        adset_dir = os.path.join(self.resource_path, "adsets")
+        for file in os.listdir(adset_dir):
+            if file.endswith(".json"):
+                print os.path.join(adset_dir, file)
+                adset = self.readAdsetFromJson(os.path.join(adset_dir, file))
+                adsets.append(adset)
+        return adsets
+
+    def createAdSet(self, adset, campaign_id):
+        adset[AdSet.Field.campaign_id] = campaign_id
+        adset.remote_create()
 
     def listAllAds(self):
         ads = []
         campaigns = self.listAllCampaigns()
         for c in campaigns:
-            ads.append(c.get_ads([Ad.Field.id, Ad.Field.name]))
+            ads.extend(c.get_ads([Ad.Field.id, Ad.Field.name]))
         return ads
 
-    def createAd(self, adset_id, page_id):
-        ad = Ad(parent_id=self.account_id)
-        ad[Ad.Field.name] = ' Matchat Ad'
-        ad[Ad.Field.adset_id] = adset_id
-
-        link_data = LinkData()
-        link_data[LinkData.Field.message] = 'Great looking SXT handbags in store. #prettybag'
-        link_data[LinkData.Field.link] = 'http://example.com'
-        link_data[LinkData.Field.caption] = 'www.example.com'
-        #link_data[LinkData.Field.image_hash] = '<IMAGE_HASH>'
-
-        call_to_action = {
-            'type': 'LEARN_MORE',
-        }
-        call_to_action['value'] = {
-            'link':'http://example.com',
-        }
-
-        link_data[LinkData.Field.call_to_action] = call_to_action
-
-        object_story_spec = ObjectStorySpec()
-        object_story_spec[ObjectStorySpec.Field.page_id] = page_id
-        object_story_spec[ObjectStorySpec.Field.link_data] = link_data
-
-        ad[Ad.Field.creative] = {
-            AdCreative.Field.title: 'try out matchat!',
-            AdCreative.Field.body: 'decentralized online marketplace',
-            #AdCreative.Field.object_url: 'https://matchat.org',
-            AdCreative.Field.object_story_spec: object_story_spec,
-            #AdCreative.Field.image_hash: image_hash,
-        }
-
-        ad[Ad.Field.status] = AdSet.Status.paused
-        ad.remote_create()
+    def readAdFromJson(self, jsonFile):
+        ad = Ad(parent_id = self.account_id)
+        config_file = open(jsonFile)
+        config = json.load(config_file)
+        config_file.close()
+        for key in config.keys():
+            ad[key] = config[key]
         return ad
 
-    def createSplitTestAdStudy(self, adset1_id, adset2_id):
+    def readAdsFromResources(self):
+        ads = []
+        ads_dir = os.path.join(self.resource_path, "ads")
+        for file in os.listdir(ads_dir):
+            if file.endswith(".json"):
+                print os.path.join(ads_dir, file)
+                adset = self.readAdFromJson(os.path.join(ads_dir, file))
+                ads.append(adset)
+        return ads
+
+    def createAd(self, ad, adset_id):
+        ad[Ad.Field.adset_id] = adset_id
+        ad['creative']['object_story_spec']['page_id'] = self.pageId
+        ad.remote_create()
+
+    def createSplitTestAdStudy(self, name, start, end, adsets):
         adstudy = AdStudy(parent_id=self.business_id)
-        adstudy[AdStudy.Field.name] = 'Matchat AdStudy'
+        adstudy[AdStudy.Field.name] = name
         adstudy[AdStudy.Field.type] = AdStudy.Type.split_test
-        adstudy[AdStudy.Field.start_time] = 1532512562
-        adstudy[AdStudy.Field.end_time] = 1532512563
-        adstudy[AdStudy.Field.cells] = [
-            {'name' : 'Men', 'adsets' : [adset1_id], 'treatment_percentage' : 50.0},
-            {'name' : 'Women', 'adsets' : [adset2_id], 'treatment_percentage' : 50.0}
-        ]
+        adstudy[AdStudy.Field.start_time] = start
+        adstudy[AdStudy.Field.end_time] = end
+        cells = []
+        for adset in adsets:
+            cell = {'name' : 'Men', 'adsets' : [adset.get_id()], 'treatment_percentage' : 1 / len(adsets) * 100}
+            cells.append(cell)
+        adstudy[AdStudy.Field.cells] = cells
         adstudy.remote_create()
         return adstudy
--- test.py
+++ test.py
@@ -9,33 +9,62 @@ if __name__ == '__main__':
     my_account_id = 'act_212526696146973'
     my_business_id = '212501799482796'
 
-    admanager = AdManager('act_212526696146973', '212501799482796')
+
+    this_dir = os.path.dirname(__file__)
+    resource_path = os.path.join(this_dir, 'resources')
+
+
+    admanager = AdManager('act_212526696146973', '212501799482796', resource_path)
 
     my_account = AdAccount(fbid=my_account_id) # Ad Account 1
     my_account.remote_read()
     print "account id: " + my_account.get_id_assured()
 
-    campaigns = admanager.listAllCampaigns()
-    print "campaigns: " + str(campaigns)
+    # campaigns = admanager.listAllCampaigns()
+    # print "campaigns: " + str(campaigns)
 
+    # campaigns = admanager.readCampaignsFromResources()
+    # print "read campaigns: " + str(campaigns)
+    #
+    # for c in campaigns:
+    #     admanager.createCampaign(c)
+    #
+    #
+    # adsets = admanager.readAdsetsFromResources()
+    # print "adsets: " + str(adsets)
+    #
+    # admanager.createAdSet(adsets[0], c.get_id())
 
     # campaign = createCampaign(my_account_id)
     # adset1 = createAdSet(my_account_id, campaign.get_id())
     # adset2 = createAdSet(my_account_id, campaign.get_id())
     # createSplitTestAdStudy(my_business_id, adset1.get_id(), adset2.get_id())
     #createAd(my_account_id, '23842999065780500', '135003680685668')
-
-    adsets = admanager.listAllAdsets()
-    print "adsets: " + str(list(adsets))
-
-    adsets = admanager.listAllAds()
-    print "ads: " + str(list(adsets))
-
-
-    # deleteAllCampaigns('act_212526696146973')
     #
-    # campaigns = listAllCampaigns('act_212526696146973')
+    adsets = admanager.listAllAdsets()
+    print "adsets: " + str(adsets)
+    #
+    # adsets = admanager.listAllAds()
+    # print "ads: " + str(adsets)
+
+
+    # campaigns = admanager.listAllCampaigns()
+    # print "campaigns: " + str(campaigns)
+    #
+    # admanager.deleteAllCampaigns()
+
+    # campaigns = admanager.listAllCampaigns()
     # print "campaigns: " + str(campaigns)
 
     # campaign = readCampaign('23842983211330500')
     # print "campaign: " + campaign[Campaign.Field.name]
+
+    ads = admanager.readAdsFromResources()
+    print "read ads: " + str(ads)
+
+    # admanager.createAd(ads[0], adsets[0].get_id())
+
+    ads = admanager.listAllAds()
+    print "ads: " + str(ads)
+
+    #adstudy = admanager.createSplitTestAdStudy("SplitTest1", 1532512562, 1532512563, adsets)
